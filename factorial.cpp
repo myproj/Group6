b050c572ab386a948f6e285c6b1090b3c061e7b8
Rohit Chauhan <rohitchira@gmail.com>
2016-08-29 16:26:25 +0530

    changed by rohit chauhan

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,12 @@
 #include "functions.h"
 
 int factorial(int n){
+	if(n<0)
+	{
+		std::cout << "Enter number again :\n";
+		std::cin >> n;
+		factorial(n);
+	} 
     if(n!=1){
 	return(n * factorial(n-1));
     }
