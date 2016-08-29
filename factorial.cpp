7148964453bc392828d1a71b516f8bdf7b0e8dd2
Rohit V <rohit_kumar@students.iitmandi.ac.in>
2016-08-29 16:15:42 +0000

    2nd modification fact.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,10 +1,10 @@
 #include "functions.h"
 using namespace std;
 int factorial(int n){
-	if(n<0)
+	if(n<0) // check for a negative number 
 		cout<<"Please enter a positive no ";
 	else{    
-	if(n!=1){
+	if(n!=1){// checking if the no is positive  or 0 but not 1
 	return(n * factorial(n-1));
     }
     //else return 1;}
