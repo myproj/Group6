088dd84b154be182e9e275ca2d12307d0076290f
Ashish Arya <goldy22arya@gmail.com>
2016-09-18 19:18:23 +0530

    Update Add_digit.c in test branch

--- Add_Digit.c
+++ Add_Digit.c
@@ -2,11 +2,14 @@
  
 int main()
 {
+	//Initialising variables.
    int n, sum = 0, remainder;
  
+ //Getting input from user
    printf("Enter an integer\n");
    scanf("%d",&n);
-    
+   
+    //Loop for getting sum of all digits.
    while(n != 0)
    {
       remainder = n % 10;
@@ -17,6 +20,7 @@ int main()
 	  printf("The n is %d\n", n);
    }
  
+ 	//printing sum of all digits.
    printf("Sum of digits of entered number = %d\n",sum);
  
    return 0;
